feeaa860706cafba55affa7b80f81fafb0766c1b
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2025-03-05 18:19:28 -0500

    Rebalance what is shown
    * Don't show files count, as I think this is absolutely negligible. Also
      unnecessarily complex.
    * Show hidden files. One more reason not show just the counts anymore.
    * Don't show git untracked files. Arguably showing hidden files is more
      relevant than showing git untracked files.

--- README.md
+++ README.md
@@ -11,9 +11,8 @@ What does it show?
     * The last command exit status only if it is not 0.
     * The number of background jobs only if there is at least one.
     * The current username, hostname and working directory.
-    * The number of visible files, followed by the number of hidden files if any.
   * The directory stack if it's not empty.
-  * `ls` output.
+  * `ls -A` output.
   * Concise `git status` only when inside a git repo.
 
 [subnixr's minimal]: https://github.com/subnixr/minimal
--- init.zsh
+++ init.zsh
@@ -10,17 +10,11 @@ _prompt_mnml_buffer-empty() {
     # draw infoline
     if (( MNML_LAST_ERR )) print -Pn '%F{${MNML_ERR_COLOR}}${MNML_LAST_ERR} '
     print -Pn '%(1j.%F{244}%j%f& .)%F{244}%n%f@%F{244}%m%f:'
-    print -Pn %F{244}${${(D)PWD}//\//%f\/%F{244}}
-    local -i a_files=$(command ls -Aq | command wc -l)
-    local -i v_files=$(command ls -q | command wc -l)
-    local -i h_files=$(( a_files - v_files ))
-    print -Pn " %f[%F{244}${v_files}%f"
-    if (( h_files )) print -Pn " (%F{244}${h_files}%f)"
-    print ]
+    print -P %F{244}${${(D)PWD}//\//%f\/%F{244}}
     # display magic enter
     if (( ${#dirstack} )) print -P %F{244}${${(D)dirstack}//\//%f\/%F{244}}%f
-    ls -F
-    command git status -sb 2>/dev/null
+    ls -AF
+    command git status -sb --untracked-files=no 2>/dev/null
     print -Pn ${PS1}
     zle redisplay
   else
